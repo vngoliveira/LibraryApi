8d106a16594b0b377de6bc0057360c55f4ed3906
Nathaly Gomes <nathaly.gomes@liferay.com>
2024-12-05 09:33:56 -0300

    LibraryApi - Remove old folders

--- library-api/routes/loanRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loanController = require('../controllers/loanController');
-
-router.post('/', loanController.createLoan);
-router.get('/', loanController.getAllLoans);
-router.put('/return/:id', loanController.returnLoan);
-
-module.exports = router;
--- library-api/routes/reportRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/reportController');
-
-router.get('/most-loaned-books', reportController.getMostLoanedBooks);
-router.get('/pending-loans', reportController.getUsersWithPendingLoans);
-
-module.exports = router;
